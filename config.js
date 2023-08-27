995c3ebc1870f40dffbb87389030f96e5598bd91
Ha Tuan Anh <109954301+tuananhha42@users.noreply.github.com>
2023-08-28 00:06:46 +0700

    None
    none

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Xin chào bé iuuu!' ,
+    introDesc: `Anh có cái này mún hỏi bé iu ạ👉👈`,
+    btnIntro: 'Dạaaa',
+    title: 'Em iuuu anh hummm? 😙',
+    desc: 'Em  không trả lời hoặc thoát ra là anh dỗi đóaa :vvvv :> ',
+    btnYes: 'Iuu nhắmmmm',
+    btnNo: 'Anh mơ tiếp đi ',
+    question:'Sao bé lại iu anhh? <3',
+    btnReply: 'Gửi cho anh iuu <3',
+    reply: 'Tại vì anh đẹp trai, tâm ý nè hihi 😚',
+    mess: 'Anh biết mà 🥰. Yêu bé lắmmm 😘😘',
+    messDesc: 'Baby cho anh hun mụt cáiii',
+    btnAccept: 'Okiiiii lun <3',
+    FbLink: 'https://www.facebook.com/stories/321526353555757/?source=profile_highlight' 
+}
